78dcf366a703a3527c90620baa517278a7275baa
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-08-23 17:36:43 +0300

    Server <> Creating Imageable table

--- server/recipies/app/Models/Recipe.php
+++ server/recipies/app/Models/Recipe.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Recipe extends Model
 {
     use HasFactory;
+
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     protected $fillable = [
         'name',
--- server/recipies/app/Models/User.php
+++ server/recipies/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -54,5 +55,10 @@ class User extends Authenticatable implements JWTSubject
 
     public function recipes(){
         return $this->hasMany(Recipe::class, 'user_id');        
+    }
+
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 }
