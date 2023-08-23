2854c670556a3e973b4ba6e2f893a88b59791f1b
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-08-23 12:37:31 +0300

    Client <> Creating basic UI Components

--- client/recipies/src/Base/ui/navbar/index.jsx
+++ client/recipies/src/Base/ui/navbar/index.jsx
@@ -1,26 +1,39 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+import Logo from'./book.png';
 
-const Navbar= () =>{
-    return (
-    <div>
-        <ul> 
-            <li>
-                <Link to="/">Dogs</Link>
-            </li>
 
-            <li>
-                <Link to="/cats">Cats</Link>
-            </li>
-            
-            <li>
-                <Link to="/sheeps">Sheeps</Link>
-            </li>
-            
-            <li>
-                <Link to="/goats">Goats</Link>
-            </li>
-        </ul>
-    </div>
-    );
-  }
-  export default Navbar;
+const Navbar = () => {
+    
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/"><img className="navbar-logo" src={Logo} alt="Logo"/></Link>
+      </div>
+      <ul className="navbar-links">
+        <li className="navbar-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/services">Services</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div>
+        {false ? 
+            <Link to="/profile" className="navbar-link">Profile</Link>
+            : 
+            <Link to="/login" className="navbar-link">Login</Link>
+        }
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
